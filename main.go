c4e0d611611d829793aaef99b19964634d819b11
mosn1bw <81889769+mosn1bw@users.noreply.github.com>
2021-05-20 05:46:37 +0430

    Update main.go

--- main.go
+++ main.go
@@ -439,6 +439,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							linebot.NewURIImagemapAction("https://store.line.me/family/play/en", linebot.ImagemapArea{0, 520, 520, 520}),
 							linebot.NewMessageImagemapAction("URANAI!", linebot.ImagemapArea{520, 520, 520, 520}),
 						),
+					)
 				} else if "mee3" == message.Text {
 					if _, err := bot.ReplyMessage(
 						replyToken,
